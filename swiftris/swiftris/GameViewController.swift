61e70d27fc346d073a5a19c5bb878ee86d436492
risachi <risachi@gmail.com>
2016-06-08 19:57:01 -0700

    init scene - titillating background

--- swiftris/swiftris/GameViewController.swift
+++ swiftris/swiftris/GameViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // configure the view
+        // as! is a forced downcast; use only when you are sure the downcast will always succeed, otherwise this form will trigger a runtime error if you try to downcast to an incorrect class type
+        // the view object is actually of type SKView but before downcasting our code treats is like a basic UIView; without downcasting, we are unable to access SKView methods and properties, such as presentScene(SKScene)
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
+        
+        // create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // present the scene
+        skView.presentScene(scene)
     }
 
     override func prefersStatusBarHidden() -> Bool {
