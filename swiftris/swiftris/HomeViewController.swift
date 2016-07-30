dfc222aeef8ab9b6df3f53055a36d24f5c6ab7d3
risachi <risachi@gmail.com>
2016-07-29 21:44:05 -0700

    authenticate player success

--- swiftris/swiftris/HomeViewController.swift
+++ swiftris/swiftris/HomeViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 import SpriteKit
-
+import GameKit
 
 
 class HomeViewController: UIViewController, SwiftrisDelegate, UIGestureRecognizerDelegate {
+    
+    func authenticateLocalPlayer(){
+        var localPlayer = GKLocalPlayer.localPlayer()
+        localPlayer.authenticateHandler = {(viewController, error) -> Void in
+            if ((viewController) != nil) {
+                self.presentViewController(viewController!, animated: true, completion: nil)
+            }else{
+                print("(GameCenter) Player authenticated: \(GKLocalPlayer.localPlayer().authenticated)")
+            }
+        }
+    }
     
     override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
         // we downcast (as!) from UIViewController to GameViewController because UIViewController doesn't have a "gameType" property, which we access below
@@ -25,7 +36,9 @@ class HomeViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        authenticateLocalPlayer();
     }
+    
     
     func gameDidEnd(swiftris: Swiftris) {}
     func gameDidBegin(swiftris: Swiftris) {}
